1bbf0a34538933beed5fe4fcdfe78c3ffb4c6e09
Gabriel de Diego <gdedi651@gmail.com>
2017-03-11 19:45:56 -0500

    update Key.cs to destroy gameObject
    Removes current gameObject when clicked

--- Assets/Udacity/Scripts/Key.cs
+++ Assets/Udacity/Scripts/Key.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Key : MonoBehaviour 
 {
+
     //Create a reference to the KeyPoofPrefab and Door
     public GameObject Door;
     public GameObject Effect;
+
+    bool collected = false;
 
 	void Update()
 	{
@@ -18,10 +21,14 @@ public class Key : MonoBehaviour
 	{
         // Instatiate the KeyPoof Prefab where this key is located
         //Instantiate.
+        Object.Instantiate(Effect, new Vector3(-8.34f, 2.644f, 106.88f), Quaternion.identity);
         // Make sure the poof animates vertically
         // Call the Unlock() method on the Door
+        //Door.
         // Set the Key Collected Variable to true
+        collected = true;
         // Destroy the key. Check the Unity documentation on how to use Destroy
+        Destroy(this.gameObject);
     }
 
 }
